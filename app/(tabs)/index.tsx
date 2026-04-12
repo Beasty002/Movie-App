6d5f0557462b370cd44ecf85348bb325241d73b7
Beasty002 <xasbin12@gmail.com>
2026-04-12 14:51:25 +0545

    feat: add tabs component

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="items-center justify-center flex-1 bg-white-500"
+    >
+      <Text className="text-5xl text-accent">Welcome.</Text>
+
+
+    </View>
+  );
+}
